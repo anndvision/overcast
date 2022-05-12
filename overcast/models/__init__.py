6c46bd80eadcd56b9078e57612ad74bd49d95a7c
Andrew Jesson <andrew.jesson@cs.ox.ac.uk>
2022-05-12 15:40:28 +0100

    add init file

---
+++ overcast/models/__init__.py
@@ -0,0 +1,5 @@
+from overcast.models.attention import DiscreteTreatmentAttentionNetwork
+from overcast.models.attention import AppendedTreatmentAttentionNetwork
+
+from overcast.models.neural_network import DiscreteTreatmentNeuralNetwork
+from overcast.models.neural_network import AppendedTreatmentNeuralNetwork

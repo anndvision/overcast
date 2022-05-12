6fc00e962834d301f757abc92f4ebdd1a40b423f
Andrew Jesson <andrew.jesson@cs.ox.ac.uk>
2022-05-12 15:35:58 +0100

    add discrete dataset to test discrete models

--- overcast/datasets/synthetic.py
+++ overcast/datasets/synthetic.py
@@ -7,7 +7,7 @@ from scipy import stats
 from torch.utils import data
 
 
-class Synthetic(data.Dataset):
+class DoseResponse(data.Dataset):
     def __init__(
         self,
         num_examples,
@@ -20,7 +20,7 @@ class Synthetic(data.Dataset):
         seed=1331,
         split=None,
     ):
-        super(Synthetic, self).__init__()
+        super(DoseResponse, self).__init__()
         rng = np.random.RandomState(seed=seed)
 
         self.num_examples = num_examples
@@ -55,7 +55,7 @@ class Synthetic(data.Dataset):
         self.r = (n / (1 - n)) / (d / (1 - d))
         self.treatments = self.treatments / 100
 
-        mu_y = f_mu(x=self.data, t=self.treatments, u=self.u, theta=gamma_y).astype(
+        mu_y = f_mu_dr(x=self.data, t=self.treatments, u=self.u, theta=gamma_y).astype(
             "float32"
         )
         eps = (sigma_y * rng.normal(size=self.data.shape)).astype("float32")
@@ -73,13 +73,14 @@ class Synthetic(data.Dataset):
         self.target_names = ["y"]
         self.treatment_names = ["t"]
 
-        self.sample_index = np.arange(len(self.data))
         if bootstrap:
-            self.sample_index = np.random.choice(self.sample_index, size=len(self.data))
-            self.data = self.data[self.sample_index]
-            self.treatments = self.treatments[self.sample_index]
-            self.targets = self.targets[self.sample_index]
-            self.u = self.u[self.sample_index]
+            sample_index = np.random.choice(
+                np.arange(len(self.data)), size=len(self.data)
+            )
+            self.data = self.data[sample_index]
+            self.treatments = self.treatments[sample_index]
+            self.targets = self.targets[sample_index]
+            self.u = self.u[sample_index]
 
     @property
     def data_frame(self) -> pd.DataFrame:
@@ -94,7 +95,7 @@ class Synthetic(data.Dataset):
         mus = []
         for treatment in treatments:
             treatment = treatment * np.ones_like(self.treatments)
-            mu_y = f_mu(x=data, t=treatment, u=self.u, theta=self.gamma_y)
+            mu_y = f_mu_dr(x=data, t=treatment, u=self.u, theta=self.gamma_y)
             mus.append(mu_y.mean())
         return np.asarray(mus)
 
@@ -103,9 +104,9 @@ class Synthetic(data.Dataset):
         mus = []
         for treatment in treatments:
             treatment = treatment * np.ones_like(self.treatments)
-            mu_y = 0.5 * f_mu(
+            mu_y = 0.5 * f_mu_dr(
                 x=data, t=treatment, u=np.ones_like(self.u), theta=self.gamma_y
-            ) + 0.5 * f_mu(
+            ) + 0.5 * f_mu_dr(
                 x=data, t=treatment, u=np.zeros_like(self.u), theta=self.gamma_y
             )
             mus.append(mu_y)
@@ -133,22 +134,122 @@ class Synthetic(data.Dataset):
         return self.data[index], self.treatments[index], self.targets[index]
 
 
-def complete_propensity(x, u, gamma, beta=0.75):
-    logit = beta * x + 0.5
-    nominal = (1 + np.exp(-logit)) ** -1
-    alpha = alpha_fn(nominal, gamma)
-    beta = beta_fn(nominal, gamma)
-    return (u / alpha) + ((1 - u) / beta)
+class CATE(data.Dataset):
+    def __init__(
+        self,
+        num_examples,
+        lambda_star,
+        p_u="bernoulli",
+        theta=4.0,
+        beta=0.75,
+        sigma_y=1.0,
+        domain=2,
+        bootstrap=False,
+        seed=1331,
+        split=None,
+    ):
+        super(CATE, self).__init__()
+        rng = np.random.RandomState(seed=seed)
+
+        self.num_examples = num_examples
+
+        self.data = rng.uniform(-domain, domain, size=(num_examples, 1)).astype(
+            "float32"
+        )
+
+        if p_u == "bernoulli":
+            self.u = rng.binomial(1, 0.5, size=(num_examples, 1)).astype("float32")
+        elif p_u == "uniform":
+            self.u = rng.uniform(size=(num_examples, 1)).astype("float32")
+        elif p_u == "beta_bi":
+            self.u = rng.beta(0.5, 0.5, size=(num_examples, 1)).astype("float32")
+        elif p_u == "beta_uni":
+            self.u = rng.beta(2, 5, size=(num_examples, 1)).astype("float32")
+        else:
+            raise NotImplementedError(f"{p_u} is not a supported distribution")
+
+        self.pi = complete_propensity(
+            x=self.data, u=self.u, _lambda=lambda_star, beta=beta
+        ).astype("float32")
+        self.treatments = rng.binomial(1, self.pi).astype("float32")
+        self.treatments = np.hstack([1 - self.treatments, self.treatments])
+
+        self.mu0 = f_mu_cate(x=self.data, t=0.0, u=self.u, theta=theta).astype(
+            "float32"
+        )
+        self.mu1 = f_mu_cate(x=self.data, t=1.0, u=self.u, theta=theta).astype(
+            "float32"
+        )
+        eps = (sigma_y * rng.normal(size=self.data.shape)).astype("float32")
+        self.y0 = self.mu0 + eps
+        self.y1 = self.mu1 + eps
+        self.targets = (
+            self.treatments[:, 1:] * self.y1 + self.treatments[:, :1] * self.y0
+        )
+        self.tau = self.mu1 - self.mu0
+
+        self.data_xfm = None
+        self.targets_xfm = None
+        self.treatments_xfm = None
+
+        self.dim_input = 1
+        self.dim_targets = 1
+        self.dim_treatments = 2
+
+        self.data_names = ["x1"]
+        self.target_names = ["y"]
+        self.treatment_names = ["t"]
+
+        if bootstrap:
+            sample_index = np.random.choice(
+                np.arange(len(self.data)), size=len(self.data)
+            )
+            self.data = self.data[sample_index]
+            self.treatments = self.treatments[sample_index]
+            self.targets = self.targets[sample_index]
+            self.u = self.u[sample_index]
+
+    @property
+    def data_frame(self) -> pd.DataFrame:
+        data = np.hstack([self.data, self.treatments, self.targets,],)
+        return pd.DataFrame(
+            data=data,
+            columns=self.data_names + self.treatment_names + self.target_names,
+        )
+
+    def __len__(self) -> int:
+        return len(self.targets)
+
+    def __getitem__(self, index) -> data.dataset.T_co:
+        return self.data[index], self.treatments[index], self.targets[index]
 
 
-def f_mu(x, t, u, theta=4.0):
+def f_mu_dr(x, t, u, theta=4.0):
     mu = t + x * np.exp(-x * t) - (theta * (u - 0.5)) * (0.5 * x + 1)
+    return mu
+
+
+def f_mu_cate(x, t, u, theta=4.0):
+    mu = (
+        (2 * t - 1) * x
+        + (2.0 * t - 1)
+        - 2 * np.sin((4 * t - 2) * x)
+        - (theta * u - 2) * (1 + 0.5 * x)
+    )
     return mu
 
 
 def f_t(x, u, lambda_star=0.02):
     mu = np.exp(x - 1) + (1 - lambda_star) * (2 * u - 1)
     return mu
+
+
+def complete_propensity(x, u, _lambda, beta=0.75):
+    logit = beta * x + 0.5
+    nominal = (1 + np.exp(-logit)) ** -1
+    alpha = alpha_fn(nominal, _lambda)
+    beta = beta_fn(nominal, _lambda)
+    return (u / alpha) + ((1 - u) / beta)
 
 
 def alpha_fn(pi, lambda_):
